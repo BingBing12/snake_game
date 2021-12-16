66dd90c8048f53f8f6fb5271941b1e1e9e04c5a7
Robin Tainton <robintainton0509@gmail.com>
2021-12-16 19:25:10 +0200

    code refactoring and implementing tail collision and snake growth

--- main.py
+++ main.py
@@ -28,9 +28,14 @@ score = Score()
 game_over = False
 while not game_over:
     screen.update()
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            score.game_over()
+            game_over = True
     if snake.head.distance(food) < 15:
         food.generate_food()
         score.increment()
+        snake.grow()
     if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
         score.game_over()
         game_over = True
--- snake.py
+++ snake.py
@@ -4,23 +4,29 @@ from turtle import Turtle
 class Snake(Turtle):
     def __init__(self):
         super().__init__()
-        self.length = 3
+        self.length = 0
         self.segments = []
-        self.pos = 0
-        self.add_segments()
+        self.starting_pos = [(0, 0), (-20, 0), (-40, 0,)]
+        self.create_snake()
         self.head = self.segments[0]
 
-    def add_segments(self):
-        for i in range(self.length):
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.setx(self.pos)
-            self.pos -= 20
-            self.segments.append(new_segment)
+    def create_snake(self):
+        for pos in self.starting_pos:
+            self.add_segment(pos)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+        self.length += 1
 
     def snake_move(self):
         for index in range(self.length - 1, 0, -1):
             next_pos = self.segments[index - 1].pos()
             self.segments[index].setpos(next_pos)
         self.head.forward(20)
+
+    def grow(self):
+        self.add_segment(self.segments[-1].position())
