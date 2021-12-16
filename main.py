420a79d9301e5725f8da4125fbd57c94191d9ba2
Robin Tainton <robintainton0509@gmail.com>
2021-12-16 18:40:00 +0200

    implementing food and score functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from score import Score
 
 
 def left():
@@ -21,11 +23,16 @@ screen.onkeypress(right, "s")
 screen.listen()
 
 snake = Snake()
-
+food = Food()
+score = Score()
 game_over = False
 while not game_over:
     screen.update()
+    if snake.head.distance(food) < 15:
+        food.generate_food()
+        score.increment()
     if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
+        score.game_over()
         game_over = True
     snake.snake_move()
     time.sleep(0.1)
---
+++ score.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.goto(0, 280)
+        self.display()
+
+    def display(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align="center")
+
+    def increment(self):
+        self.score += 1
+        self.display()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center")
